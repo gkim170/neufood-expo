c294aeda76ab9b560a0e05806171558a222d1ff1
wuw225 <wuw225@lehigh.edu>
2024-11-12 23:28:54 -0500

    password appears to be *'s, edit button in allergies

--- expo/app/(tabs)/(profile)/allergies.tsx
+++ expo/app/(tabs)/(profile)/allergies.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import React, { useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import CustomButton from '../../../components/CustomButton';
@@ -12,16 +12,74 @@ const allergies = () => {
     console.log('Allergies page rendered');
   }, []);
 
+const handleEdit = () => {
+  console.log('edit allergies...'); 
+    
+    // // Navigate to edit profile
+    // navigation.navigate('editprofile', { 
+    //   user,
+    //   updateUser: (updatedUser) => setUser(updatedUser),
+    //  });
+}
 
   return (
     <View className="flex-1 justify-center items-center bg-custom-background">
-      <BackArrow/>
-      <Text className="font-bold text-2xl my-4">
-        Allergies
-      </Text>
+      <View style={styles.container}>
+        <View style={styles.back}>
+            <BackArrow/>
+        </View>
+        <TouchableOpacity
+            style={styles.editbutton}
+            onPress={handleEdit}>
+              <Text style={styles.textedit}>Edit</Text>
+        </TouchableOpacity>
+      </View>
+
+
+      <View style={styles.body}>
+        <Text className="font-bold text-2xl my-4" style={styles.title}>
+          My Allergies
+        </Text>
+        <View style={styles.allergybuttons}>
+
+        </View>
+      </View>
     </View>
   );
 };
 
 
 export default allergies;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',          
+    justifyContent: 'space-between', 
+    alignItems: 'center',           
+    paddingHorizontal: 10,
+    height: 60,                     
+    marginTop: 5,
+  },
+  back: {
+    flex: 1,
+    padding: 10,
+    marginBottom: 30,
+  },  
+  editbutton: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 30, 
+  },
+  textedit: {
+    textAlign: 'right',
+  },
+  body: {
+    flex: 7,
+    paddingTop: 40,
+  },
+  title: {
+    textAlign: 'left',
+  },
+  allergybuttons: {},
+})
